2048f6e87b783381e3315668a4b8d3e71d1e9c85
Nigel Marowa <nigelmarowa9@gmail.com>
2019-08-02 16:21:12 +0100

    Updated template.py

--- Prac1/template.py
+++ Prac1/template.py
@@ -3,31 +3,22 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: <Nigel Marowa>
+Student Number: <MRWNIG002>
+Prac: <Prac 1>
+Date: <29/07/2019>
 """
-
 # import Relevant Librares
+# import Librares
 import RPi.GPIO as GPIO
+from time import sleep
+from itertools import product
 
-# Logic that you write
-def main():
-    print("write your logic here")
+#LEDs pin out
+outputpin = [13, 31, 37] 
 
+#pin out for the button pins
+inputpin = [15, 29] 
 
-# Only run the functions if 
-if __name__ == "__main__":
-    # Make sure the GPIO is stopped correctly
-    try:
-        while True:
-            main()
-    except KeyboardInterrupt:
-        print("Exiting gracefully")
-        # Turn off your GPIOs here
-        GPIO.cleanup()
-    except Exception as e:
-        GPIO.cleanup()
-        print("Some other error occurred")
-        print(e.message)
+# outputs showing the list for the 3-bit counter
+listCounter =list(product([0,1], repeat=3)) 
